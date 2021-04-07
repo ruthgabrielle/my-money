b294f6336ffe271cca8873aa883da68adfeb74a3
ruthgabrielle <ruthfgms@gmail.com>
2021-04-07 10:47:58 -0300

    Adicionando Bootstrap ao projeto e listagem de meses

--- src/App.js
+++ src/App.js
@@ -1,32 +1,83 @@
 import React from  'react'
-import useGet from './useGet'
-import usePost from './usePost'
-import useDelete from './useDelete'
+import Rest from './rest'
 
-const url = 'https://mymoney-dev-ruth-default-rtdb.firebaseio.com/movimentacoes/2021-03.json'
+
+const baseURL = 'https://mymoney-dev-ruth-default-rtdb.firebaseio.com/'
+const { useGet, usePost, useDelete } = Rest(baseURL)
 
 function App() {
-  const data = useGet(url)
-  const [postData, post] = usePost(url)
-  const [deleteData, remove] = useDelete()
+//  const data = useGet('movimentacoes/2021-03')
+    const data = useGet('meses')
+//  const [postData, post] = usePost('movimentacoes/2021-03')
+//  const [deleteData, remove] = useDelete()
 
   const saveNew = () => {
-    post({ valor: 10, descricao: 'testinho'})
+    //post({ valor: 10, descricao: 'testinho'})
   }
 
   const doRemove = () => {
-    remove('https://mymoney-dev-ruth-default-rtdb.firebaseio.com/movimentacoes/2021-03/-MXS5jgqk9WRED5LVpQv.json')
+    //remove('movimentacoes/2021-03/-MXbRH1hI8HBOCZKSmJN')
   }
+
   return (
     <div>
-      <h1>MyMoney</h1>
-      {JSON.stringify(data)}
-      { data.loading && <p>Loading...</p> }
-      <button onClick={saveNew}>Salvar</button>
-      <pre>{JSON.stringify(postData)}</pre>
-      <button onClick={doRemove}>Deletar</button>
-      <pre>{JSON.stringify(deleteData)}</pre>
+    <nav className='navbar navbar-light bg-light'>
+      <div className='container'>
+        <a className='navbar-brand'>MyMoney</a>
+      </div>
+    </nav>
 
+    <h2>Adicionar mês</h2>
+    <select>
+      <option value='2021'>2021</option>
+      <option value='2022'>2022</option>
+    </select>
+    <select>
+      <option value='03'>03</option>
+      <option value='04'>04</option>
+      <option value='05'>05</option>
+    </select>
+    
+    <div className='container'>
+      <button>Adicionar mês</button>
+        {
+          data.loading && <span>Carregando...</span>
+        }
+        {
+        !data.loading && (
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>Mês</th>
+                <th>Previsão de Entradas</th>
+                <th>Entradas</th>
+                <th>Previsão de Saídas</th>
+                <th>Saída</th>
+              </tr>
+            </thead>
+            <tbody>
+            {
+              Object
+              .keys(data.data)
+              .map(mes => {
+                return (
+                  <tr key={mes}>
+                    <td>{mes}</td>
+                    <td>{data.data[mes].previsao_ent}</td>
+                    <td>{data.data[mes].entradas}</td>
+                    <td>{data.data[mes].previsao_saida}</td>
+                    <td>{data.data[mes].saida}</td>
+                  </tr>
+                )
+              })
+            }
+            </tbody>
+          </table>
+        )
+      }
+    </div>
+
+    <pre>{JSON.stringify(data)}</pre>
     </div>
   );
 }
