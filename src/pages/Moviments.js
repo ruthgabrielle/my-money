10de856908d0228291ff7e7e0a4d577462b374fd
ruthgabrielle <ruthfgms@gmail.com>
2021-04-12 15:39:09 -0300

    Adicionar e remover movimentação / Refetch

--- src/pages/Moviments.js
+++ src/pages/Moviments.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import {React, useState} from 'react'
 import Rest from '../utils/rest'
 
 const baseURL = 'https://mymoney-dev-ruth-default-rtdb.firebaseio.com/'
-const { useGet } = Rest(baseURL)
+const { useGet, usePost, useDelete } = Rest(baseURL)
 
 
 const Moviments = ({match}) => {
    const data = useGet(`movimentacoes/${match.params.data}`)
-   
+   const [postData, save] = usePost(`movimentacoes/${match.params.data}`)
+   const [removeData, remover] = useDelete()
+   const [descricao, setDescricao] = useState('')
+   const [valor, setValor] = useState(0.0)
+
+
+   const onChangeDescricao = evt => {
+      setDescricao(evt.target.value)
+   }
+
+   const onChangeValor = evt => {
+      setValor(parseFloat(evt.target.value))
+   }
+   const saveMoviment = async() => {
+      await save({
+         descricao,
+         valor
+      })
+      setDescricao('')
+      setValor(0)
+      data.refetch()
+   }
+
+   const removeMoviment = async(id) => {
+      await remover (`movimentacoes/${match.params.data}/${id}`)
+      data.refetch()
+   }
+   //controlled form
    return(
    <div className='container'> 
    <h1>Moviments</h1>
@@ -28,10 +55,20 @@ const Moviments = ({match}) => {
                      <tr>
                         <td>{data.data[movimentacao].descricao}</td>
                         <td>{data.data[movimentacao].valor}</td>
+                        <button onClick={() => removeMoviment(movimentacao)}>-</button>
                      </tr>
                   )
                })
          }
+         <tr>
+            <td>
+               <input type='text' value={descricao} onChange={onChangeDescricao}/>
+            </td>
+            <td> 
+               <input type='text' value={valor} onChange={onChangeValor}/>  
+               <button onClick={saveMoviment}>+</button> 
+            </td>
+         </tr>
       </tbody>
    </table>
    </div>
--- src/utils/rest.js
+++ src/utils/rest.js
@@ -27,43 +27,43 @@ const reducer = (state, action) => {
 const init = baseURL => {
     const useGet = resource => {
         const [data, dispatch] = useReducer(reducer, INITIAL_STATE)
+        const carregar = async() => {
+            dispatch({type:  'REQUEST'})
+            const res = await axios
+                .get(baseURL + resource + '.json')
+            dispatch({type: 'SUCCESS', data: res.data})
+        }
         useEffect(() => {
-          dispatch({type:  'REQUEST'})
-          axios
-            .get(baseURL + resource + '.json')
-            .then(res => {
-              dispatch({type: 'SUCCESS', data: res.data})
-            })
-        }, []) 
-        //os colchetes são utilizados pois a função é chamada somente uma vez e não depende de ninguém
-        return data
+            carregar() 
+        }, [resource]) 
+        return {
+            ...data,
+            refetch: carregar
+        }
     }
     const usePost = (resource) => {
         const [data, dispatch] = useReducer(reducer, INITIAL_STATE)
-        const post = data => {
+        const post = async (data) => {
             dispatch({type: 'REQUEST'})
-            axios
-            .post(baseURL + resource + '.json', data)
-            .then(res => {
-                dispatch({
-                    type: 'SUCCESS',
-                    data: res.data
-                })
+            const res = await axios.post(baseURL + resource + '.json', data)
+            dispatch({
+                type: 'SUCCESS',
+                data: res.data
             })
         }
+        
         return [data, post]
     }
+
     const useDelete = () => {
         const [data, dispatch] = useReducer(reducer, INITIAL_STATE)
-        const remove = resource => {
+        const remove = async (resource) => {
             dispatch({type: 'REQUEST'})
-            axios
-            .delete(baseURL + resource + '.json')
-            .then(() => {
-                dispatch({
+            await axios
+             .delete(baseURL + resource + '.json')
+              dispatch({
                     type: 'SUCCESS'
                 })
-            })
         }
         return [data, remove]
     }
